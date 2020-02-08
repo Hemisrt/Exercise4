9c2a84b311e2d7b8e303e95a9c000d938e927f4b
trentonjavadroid1660 <59889828+trentonjavadroid1660@users.noreply.github.com>
2020-02-08 00:59:08 -0500

    Update Main.java

--- src/com/company/Greenlee/Main.java
+++ src/com/company/Greenlee/Main.java
@@ -1,64 +1,79 @@
 package com.company.Greenlee;
 
-import java.util.*;
+import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Main {
-
+    // Trenton Greenlee.
+    // 2/4/2020.
+    // Exercise 4.
     public static void main(String[] args) {
-        /*Write a program that first prompts the user to enter a list of city names, one
-        at a time until the user specifies "END". Next, the program should prompt the
-        user to enter the average daily temperature for each of the next five days for
-        each city. The program should store the user's cities and temperature data in
-        one data structure. Next, calculate the five-day average for each city.
-        Finally, display a message containing the city's name and the average for each
-        city.
-        The code to prompt the user for input, to calculate the five-day average, and to
-        display the results should be in separate methods.
-        Hint: You can use String.split(" ") to split a string into an array of strings
-        based on where spaces occur in the original string.*/
+        // Calling the city() and output() methods.
         city();
-        averageTemp();
         output();
     }
 
+    // Global Scanner (Within the program).
     private static Scanner input = new Scanner(System.in);
 
+    // Global HashMap and ArrayLists (Within the program).
     private static Map<String, Double> cityAvgTemp = new HashMap<>();
-    private static List<Double> calc = new ArrayList<>();
-    private static List<Double> avg = new ArrayList<>();
     private static List<String> city = new ArrayList<>();
+    private static List<Double> avg = new ArrayList<>();
 
+    // Methods.
+    // city() - User inputs the name of a city and the 5 day temperatures.
     public static void city() {
+        // Two separate Scanners for each input variable.
         Scanner input = new Scanner(System.in);
         Scanner temper = new Scanner(System.in);
-        System.out.println("Please enter a city names: ");
+        System.out.println("Please enter a city names (Enter 'END' to stop): ");
+        double totalTemp = 0;
+        double temp;
         String user = input.nextLine();
 
+        // Using a while loop to have the user continue inputting until they enter 'END' for the user variable.
         while (!(user.equals("END"))) {
             System.out.println("Enter 5-Day Temperature: ");
             for (int i = 0; i < 5; i++) {
-                double temp = temper.nextDouble();
-                double totalTemp = 0;
-                totalTemp = temp + totalTemp;
-                calc.add(totalTemp);
+                temp = temper.nextDouble();
+                totalTemp += temp;
             }
+            // Calling averageTemp() method to divide the total by the number of temperatures inputted which is 5.
+            averageTemp(totalTemp);
+            totalTemp = 0;
+            // Adding user variable to the city ArrayList, indicating the average temperature has been calculated.
             city.add(user);
-            System.out.println("Please enter a city names: ");
+            // Asking user for city again since previous city input did not equal 'END'.
+            System.out.println("Please enter a city names (Enter 'END' to stop): ");
             user = input.nextLine();
         }
     }
 
-    public static void averageTemp() {
-        double average = 0;
-        for (int i = 0; i < calc.size(); i++) {
-            average = calc.get(i) / 5;
-            calc.set(i, average);
-        }
+    // averageTemp() - Calculates the average temperature of the 5-Day week and adds the value to the avg ArrayList.
+    public static void averageTemp(double total) {
+        double average = total / 5.0;
+        avg.add(average);
     }
 
+    // output() - Prints out the city's name and average temperature.
+    // Not guaranteed to have the same order the user inputted the city name and its average temperature.
     public static void output() {
-        for (int i = 0; i < city.size(); i++) {
-            System.out.println(cityAvgTemp.put(city.get(i), avg.get(i)));
+        // Using a for loop to connect the city and its average temperature by their corresponding indexes.
+        for (int i = 0; i < avg.size(); i++) {
+            cityAvgTemp.put(city.get(i), avg.get(i));
+        }
+
+        // Using the DecimalFormat class to restrict the decimal places to two for the average temperature.
+        DecimalFormat celsiusFormat = new DecimalFormat("###.##");
+
+        // Using a for-each loop to separate the key-value pair into an organized output.
+        for (Map.Entry<String, Double> entry : cityAvgTemp.entrySet()) {
+            System.out.println("City: " + entry.getKey() + ", Average Temperature: " + celsiusFormat.format(entry.getValue()) + "°C");
         }
     }
 }
